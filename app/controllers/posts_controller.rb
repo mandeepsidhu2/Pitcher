4d10512d7cf319e11f93b74feb4855919d12a41b
Amadhya Anand <eshuanand07@gmail.com>
2018-06-28 04:26:18 +0530

    Added new,create action in post controller and modified index file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
+  	@post=Post.all
   end
   def new 
+  	@post=Post.new
+  end
+  def create
+  	@post=Post.create(title:params[:post][:title],content:params[:post][:content])
+  	redirect_to action: 'index'
   end
 end
