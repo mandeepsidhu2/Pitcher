f4dce3294c77bbc6f743689c76675de862d289ce
Amadhya Anand <eshuanand07@gmail.com>
2018-06-28 17:19:15 +0530

    Added delete and create functionality for log in users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,10 +7,16 @@ class PostsController < UsersController
   end
   def new 
   	@post=Post.new
+    if session[:user_id].blank?
+      redirect_to action: 'index'
+    end
   end
   def create
-  	@post=Post.create(title:params[:post][:title],content:params[:post][:content])
-  	redirect_to action: 'index'
+    if !session[:user_id].blank?
+        @signed_in_user=User.find(session[:user_id])
+  	   @signed_in_user.posts.create(title:params[:post][:title],content:params[:post][:content])
+  	   redirect_to action: 'index'
+     end
   end
   def destroy 
   	@post=Post.find(params[:id])
